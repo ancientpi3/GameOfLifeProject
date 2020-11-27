6ea4061a6babb2d7d41442fb36df8e2c92ebfc7c
Ethan Bevan <EBEVAN01@rams.shepherd.edu>
2020-11-27 15:12:45 -0500

    Added GameModel functionality

--- GameOfLifeProject/GameOfLifeProject.py
+++ GameOfLifeProject/GameOfLifeProject.py
@@ -84,21 +84,46 @@ import numpy
 #drawTk.destroy()
 
 class GameModel:
+    
+    SET=[[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
+    
     #Initialize a gamemodel with cell width and height.
     def __init__(self,width,height):
-        print("stub")
+        self.width = width
+        self.height = height
+        self.gridState=numpy.array([[0]*(width+1)]*(height+1))
     
     #Loops through copy of gamestate array and updates gamestate according to rules.
     def updateState(self):
-        print("stub")
+        oldState = self.gridState.copy()
+        for x in range(1,self.width):
+            for y in range(1,self.height):
+
+                #Observe surrounding cells
+                adj = [oldState[x+self.SET[0][0]][y+self.SET[0][1]]]
+                for L in range(1,8):
+                    adj = adj+[oldState[x+self.SET[L][0]][y+self.SET[L][1]]]
+                    
+                #Change state based on rules
+                if oldState[x][y]==0:
+                    if sum(adj) == 3:
+                        self.gridState[x][y]=1
+                else:
+                    if sum(adj)<=1 or sum(adj)>=4:
+                        self.gridState[x][y]=0
     
     #Returns true if the cell is live and false if the cell is dead
     def cellIsLive(self,x,y):
-        return "stub"
+        return bool(self.gridState[x][y])
     
     #Activates cell if it is dead, Deactivates cell if it is alive.
     def activateCell(self,x,y):
-        print("stub")
+        if bool(self.gridState[x][y]):
+            self.gridState[x][y] = 0
+        else:
+            self.gridState[x][y] = 1
+
+
 class GameView():
     
     hexx = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","10"]
