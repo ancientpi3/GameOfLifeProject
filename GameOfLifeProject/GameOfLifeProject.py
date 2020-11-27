134bf4881d1d8a7f4c0d6d80be338895cbffca81
Ethan Bevan <EBEVAN01@rams.shepherd.edu>
2020-11-27 15:02:42 -0500

    Commented out monolith

--- GameOfLifeProject/GameOfLifeProject.py
+++ GameOfLifeProject/GameOfLifeProject.py
@@ -7,78 +7,78 @@ from tkinter import Tk, Canvas, PhotoImage, mainloop
 #from math import sin
 import copy
 import time
-#import random
+import random
 import numpy
-hexx = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","10"]
-def NumToHex(num):
-    return hexx[int(((num-(num%16))/16))]+hexx[int(num%16)]
-Boarder = 5
+#hexx = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","10"]
+#def NumToHex(num):
+#    return hexx[int(((num-(num%16))/16))]+hexx[int(num%16)]
+#Boarder = 5
 
-PXW = 25
-WINW = 500
-WINH = 500 
-Yellow = "#ffff00"
-PXL_W = int(WINW/PXW)
-PXL_H = int(WINH/PXW)
-Key = True
-
-b = [0]*(PXL_W+1)
-d = [b]*(PXL_H+1)
-
-gridState=numpy.array(d)
-
-def callback(event):
-    global gridState
-    clickX = int(event.x/PXW)
-    clickY = int(event.y/PXW)
-    #print(clickX," ",clickY)
-    drawCanv.create_rectangle(clickX*PXW,clickY*PXW,clickX*PXW+PXW,clickY*PXW+PXW,fill = "#"+NumToHex(int((clickX/PXL_H)*255))+NumToHex(int((clickY/PXL_W)*255))+NumToHex(255))
-    gridState[clickX][clickY] = 1
-
-def Update(char):
+#PXW = 25
+#WINW = 500
+#WINH = 500 
+#Yellow = "#ffff00"
+#PXL_W = int(WINW/PXW)
+#PXL_H = int(WINH/PXW)
+#Key = True
+#
+#b = [0]*(PXL_W+1)
+#d = [b]*(PXL_H+1)
+#
+#gridState=numpy.array(d)
+#
+#def callback(event):
+#    global gridState
+#    clickX = int(event.x/PXW)
+#    clickY = int(event.y/PXW)
+#    #print(clickX," ",clickY)
+#    drawCanv.create_rectangle(clickX*PXW,clickY*PXW,clickX*PXW+PXW,clickY*PXW+PXW,fill = "#"+NumToHex(int((clickX/PXL_H)*255))+NumToHex(int((clickY/PXL_W)*255))+NumToHex(255))
+#    gridState[clickX][clickY] = 1
+#
+#def Update(char):
     global Key
     Key = False
 
-drawTk = Tk()
-drawCanv = Canvas(drawTk, width=WINW, height=WINH, bg="#000000")
-drawCanv.bind("<ButtonPress>", callback)
-drawCanv.focus_set()
-drawCanv.bind('<Key>', Update)
-drawCanv.pack()
+#drawTk = Tk()
+#drawCanv = Canvas(drawTk, width=WINW, height=WINH, bg="#000000")
+#drawCanv.bind("<ButtonPress>", callback)
+#drawCanv.focus_set()
+#drawCanv.bind('<Key>', Update)
+#drawCanv.pack()
 
 
 
-SET=[[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
-            
-while True:
-    if Key:
-        drawTk.update()
-    else:
-        drawCanv.delete("all")
-        oldState = gridState.copy()
-
-        for x in range(1,PXL_W):
-            for y in range(1,PXL_H):
-                adj = [oldState[x+SET[0][0]][y+SET[0][1]]]
-                for L in range(1,8):
-                    adj = adj+[oldState[x+SET[L][0]][y+SET[L][1]]]
-
-                if oldState[x][y]==0:
-
-                    if sum(adj) == 3:
-                        gridState[x][y]=1
-                        
-
-
-                else:
-                    if sum(adj)<=1 or sum(adj)>=4:
-                        gridState[x][y]=0
-
-
-        for x in range(PXL_W-1):
-            for y in range(PXL_H-1):
-                if gridState[x][y] == 1:
-                    drawCanv.create_rectangle(x*PXW,y*PXW,x*PXW+PXW,y*PXW+PXW,fill = "#"+NumToHex(int((x/PXL_H)*255))+NumToHex(int((y/PXL_W)*255))+NumToHex(255))
-        Key = True
-        
-drawTk.destroy()
+#SET=[[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
+#            
+#while True:
+#    if Key:
+#        drawTk.update()
+#    else:
+#        drawCanv.delete("all")
+#        oldState = gridState.copy()
+#
+#        for x in range(1,PXL_W):
+#            for y in range(1,PXL_H):
+#                adj = [oldState[x+SET[0][0]][y+SET[0][1]]]
+#                for L in range(1,8):
+#                    adj = adj+[oldState[x+SET[L][0]][y+SET[L][1]]]
+#
+#                if oldState[x][y]==0:
+#
+#                    if sum(adj) == 3:
+#                        gridState[x][y]=1
+#                        
+#
+#
+#                else:
+#                    if sum(adj)<=1 or sum(adj)>=4:
+#                        gridState[x][y]=0
+#
+#
+#        for x in range(PXL_W-1):
+#            for y in range(PXL_H-1):
+#                if gridState[x][y] == 1:
+#                    drawCanv.create_rectangle(x*PXW,y*PXW,x*PXW+PXW,y*PXW+PXW,fill = "#"+NumToHex(int((x/PXL_H)*255))+NumToHex(int((y/PXL_W)*255))+NumToHex(255))
+#        Key = True
+#        
+#drawTk.destroy()
