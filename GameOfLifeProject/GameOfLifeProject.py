398899c9a2f5a176bc4d8a4e31109816fe2112cc
Ethan Bevan <EBEVAN01@rams.shepherd.edu>
2020-12-04 11:55:17 -0500

    Cleaned up Original for video presentation

--- GameOfLifeProject/GameOfLifeProject.py
+++ GameOfLifeProject/GameOfLifeProject.py
@@ -1,13 +1,6 @@
-
-##Births: Each dead cell adjacent to exactly three live neighbors will become live in the next generation.
-##Death by isolation: Each live cell with one or fewer live neighbors will die in the next generation.
-##Death by overcrowding: Each live cell with four or more live neighbors will die in the next generation.
-##Survival: Each live cell with either two or three live neighbors will remain alive for the next generation.
 from tkinter import Tk, Canvas, PhotoImage, mainloop
-#from math import sin
 import copy
 import time
-#import random
 import numpy
 hexx = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","10"]
 def NumToHex(num):
